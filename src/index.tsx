baf255fe30e77347007d4e963321a92fb8c7b546
류호진 <interarea@naver.com>
2022-04-10 18:02:50 +0900

    react 18 대응

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-const render = ReactDOM.render; render(<App />, document.getElementById('root'));
+import { createRoot } from "react-dom/client";
+import App from "./App";
+const container = document.getElementById("root");
+const root = createRoot(container as Element);
+root.render(<App />);
